c6fb7bc910e8cd9c1a4479f1570b1fec0f8052ce
Fernando <feh.wilinando@gmail.com>
2019-06-22 09:27:12 -0300

    Setup project

---
+++ calendar/src/main/java/br/com/caelum/garagesharing/calendar/CalendarApplication.java
@@ -0,0 +1,13 @@
+package br.com.caelum.garagesharing.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarApplication.class, args);
+    }
+
+}
