bac2ba9acbd0a45020aeb8201f1d25c1fed84f6e
Fernando <feh.wilinando@gmail.com>
2019-06-22 12:01:19 -0300

    Fix mapping of garage and unavailability, implement configurations

--- calendar/application/src/main/java/br/com/caelum/garagesharing/calendar/shared/domain/Unavailability.java
+++ calendar/application/src/main/java/br/com/caelum/garagesharing/calendar/shared/domain/Unavailability.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -16,7 +17,7 @@ public class Unavailability {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToMany
+    @ManyToOne
     private Garage garage;
 
     @NotNull
