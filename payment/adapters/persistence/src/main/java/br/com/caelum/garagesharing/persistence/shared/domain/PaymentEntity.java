462010bf2d0cc87d9ee05444b1a73971e3c72bbc
Fernando <feh.wilinando@gmail.com>
2019-06-26 09:34:07 -0300

    Fix typo

--- payment/adapters/persistence/src/main/java/br/com/caelum/garagesharing/persistence/shared/domain/PaymentEntity.java
+++ payment/adapters/persistence/src/main/java/br/com/caelum/garagesharing/persistence/shared/domain/PaymentEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "paymets")
+@Table(name = "payments")
 public class PaymentEntity {
 
     @Id
